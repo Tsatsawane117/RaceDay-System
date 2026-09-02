ed7dc057c591a8895bb7992ad1669c3b3989416b
Phindulo Nekhofhe <ST10451997@rcconnect.edu.za>
2026-09-02 21:17:28 +0200

    sample enrolments linking Participants to Categories

---
+++ SQLQuery7.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.Enrolments (ParticipantID, CategoryID, BibNumber, Status) VALUES
+(3, 2, 'B1001', 'Confirmed'),
+(3, 4, 'B1002', 'Confirmed'),
+(4, 1, 'B1003', 'Confirmed'),
+(4, 5, 'B1004', 'Pending');
