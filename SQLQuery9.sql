3797e69b3753523732fc54a0d226ef339d95ffba
Phindulo Nekhofhe <ST10451997@rcconnect.edu.za>
2026-09-02 21:22:45 +0200

    sample payments methods

---
+++ SQLQuery9.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.Payments (EnrolmentID, Amount, PaymentMethod, PaymentStatus) VALUES
+(1, 250.00, 'Card', 'Success'),
+(2, 150.00, 'EFT', 'Success'),
+(3, 350.00, 'Card', 'Success'),
+(4, 180.00, 'Card', 'Pending');
