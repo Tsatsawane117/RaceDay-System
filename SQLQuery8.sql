a1f61c070813ad7f70cfab99a15b5b3603da840f
Phindulo Nekhofhe <ST10451997@rcconnect.edu.za>
2026-09-02 21:20:10 +0200

    sample results for confirmed enrolments

---
+++ SQLQuery8.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.Results (EnrolmentID, FinishTime, Position, Status) VALUES
+(1, '01:52:34', 12, 'Finished'),
+(2, '00:48:10', 5, 'Finished'),
+(3, NULL, NULL, 'Not Started'),
+(4, NULL, NULL, 'Not Started');
