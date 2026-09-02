f85be83a29c01cf640c816cecc9e3bf6fdc2dd63
Phindulo Nekhofhe <ST10451997@rcconnect.edu.za>
2026-09-02 21:16:00 +0200

    at least one category per event

---
+++ SQLQuery6.sql
@@ -0,0 +1,7 @@
+INSERT INTO dbo.Categories (EventID, CategoryName, DistanceKM, MaxParticipants, EntryFee) VALUES
+(1, '42km Full Marathon', 42.20, 500, 350.00),
+(1, '21km Half Marathon', 21.10, 800, 250.00),
+(2, '5km Fun Run', 5.00, 1000, 100.00),
+(2, '10km Fun Run', 10.00, 600, 150.00),
+(3, 'Beginner Trail (8km)', 8.00, 200, 180.00),
+(3, 'Advanced Trail (18km)', 18.00, 150, 280.00);
