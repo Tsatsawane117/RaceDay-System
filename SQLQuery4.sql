8d4dcf8acd3acadd414f4f43f241e53c726c2a66
Phindulo Nekhofhe <ST10451997@rcconnect.edu.za>
2026-09-04 09:42:34 +0200

    Delete SQLQuery4.sql

--- SQLQuery4.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO dbo.Users (FullName, Email, PasswordHash, Role, ContactNumber) VALUES
-('Thabo Mokoena', 'thabo.mokoena@raceday.co.za', 'hashed_pw_1', 'Organiser', '0821234567'),
-('Lindiwe Nkosi', 'lindiwe.nkosi@raceday.co.za', 'hashed_pw_2', 'Organiser', '0837654321'),
-('Sipho Dlamini', 'sipho.dlamini@gmail.com', 'hashed_pw_3', 'Participant', '0731112222'),
-('Amahle Zulu', 'amahle.zulu@gmail.com', 'hashed_pw_4', 'Participant', '0743334444');
