26232459688c6a8a3f3fdf4a13502335ded844c2
Phindulo Nekhofhe <ST10451997@rcconnect.edu.za>
2026-09-04 09:41:46 +0200

    Delete SQLQuery7.sql

--- SQLQuery7.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO dbo.Enrolments (ParticipantID, CategoryID, BibNumber, Status) VALUES
-(3, 2, 'B1001', 'Confirmed'),
-(3, 4, 'B1002', 'Confirmed'),
-(4, 1, 'B1003', 'Confirmed'),
-(4, 5, 'B1004', 'Pending');
