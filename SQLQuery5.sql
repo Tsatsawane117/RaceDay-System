4bd3b8b22e40032e5c69ba898458eb683d585afb
Phindulo Nekhofhe <ST10451997@rcconnect.edu.za>
2026-09-04 09:42:13 +0200

    Delete SQLQuery5.sql

--- SQLQuery5.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO dbo.Events (OrganiserID, EventName, EventDate, Location, Description, Status) VALUES
-(1, 'Johannesburg City Marathon', '2026-09-12', 'Sandton, Johannesburg', 'Annual city marathon through the Johannesburg CBD and Sandton.', 'Open'),
-(1, 'Soweto Fun Run', '2026-10-03', 'Soweto, Johannesburg', 'Community fun run supporting local youth sports programmes.', 'Open'),
-(2, 'Pretoria Trail Challenge', '2026-11-15', 'Groenkloof Nature Reserve, Pretoria', 'Off-road trail race across three difficulty categories.', 'Planned');
