5f5f4a734b4c1bc174b3c126a51e2e032549666a
Phindulo Nekhofhe <ST10451997@rcconnect.edu.za>
2026-09-04 09:40:54 +0200

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,6 +0,0 @@
-IF OBJECT_ID('dbo.Payments', 'U') IS NOT NULL DROP TABLE dbo.Payments;
-IF OBJECT_ID('dbo.Results', 'U') IS NOT NULL DROP TABLE dbo.Results;
-IF OBJECT_ID('dbo.Enrolments', 'U') IS NOT NULL DROP TABLE dbo.Enrolments;
-IF OBJECT_ID('dbo.Categories', 'U') IS NOT NULL DROP TABLE dbo.Categories;
-IF OBJECT_ID('dbo.Events', 'U') IS NOT NULL DROP TABLE dbo.Events;
-IF OBJECT_ID('dbo.Users', 'U') IS NOT NULL DROP TABLE dbo.Users;
